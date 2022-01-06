28288fcf73902f06792bf401cf09f04cc3ffb6a1
Yuval Tal <Yuval.Tal@Kornit.com>
2022-01-06 23:23:52 +0200

    added ErrorBoundary around <App/>

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.render(
-    <App />,
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>,
   document.getElementById('root'
   )
 );
